c736a26cc8ada6b984f77a4f43e9f0b0067e9be2
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-07-09 23:10:26 +0600

    Add files via upload

--- B_Astrophysicists.cpp
+++ B_Astrophysicists.cpp
@@ -1,14 +1,12 @@
 /*
- 
                Starting with the name of almighty ALLAH 
 *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**
             In the name of Allah, Most Gracious, Most Merciful             
-                   লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ 
+                    লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ 
                         Author:  tarek200 
-                        Created: 19.06.2023 
+                        Created: 07.07.2023 
+                    United International University
                 Practice is the only shortcut to improve  
- 
- 
 */
 #include <bits/stdc++.h>
 #include<vector>
@@ -31,41 +29,51 @@
 #define   NO              cout<<"NO"<<endl;
 #define   exit            return ;
 #define   next            continue;
-#define   B               break;
 #define   checkmate       return 0;
 #define   nl              "\n"
 #define   gcd(a,b)        __gcd(a,b)
 #define   lcm(a,b)        (a/gcd(a,b))*b
 #define   mod             1000000007
-#define   PI              3.14159265358979323846
-#define   all(x)         (x).begin(), (x).end()
-#define   des(x)         (x).rbegin(), (x).rend()
+#define   all(x)          (x).begin(), (x).end()
+#define   des(x)          (x).rbegin(), (x).rend()
 #define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
 #define   cinv(v)         for(auto &it:v)cin>>it;
 #define   FOR(i,a,b)      for(ll i=a;i<b;i++)
 #define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
 #define   LOOP2(i,a,b)    for(ll i=a;i<=b;i++)
-#define   ROF(i, a, b)    for (ll i = a; i >b; i--)
+#define   ROF(i, a, b)    for (ll i = a; i >=b; i--)
 const long long int N     = (ll) 1e7+1;
 const long long int M     = (ll) 1e9+5;
+#define MAX                 200005
+#define MOD                 1000003
+#define eps                 1e-9
+#define INF                 0x3f3f3f3f3f3f3f3f // 4,557,430,888,798,830,399
+#define inf                 0x3f3f3f3f // 1,061,109,567
 using namespace std;
 void run(){
-    ll n ,k;
-    cin >> n >> k;
-    ll diff= abs(n-k);
-    ll step=0;
-    if(n>k){
-        ll x = n/2;
-        ll y=(x-0)-(diff-x);
-        step=abs(y);
-
-    }
-    else if (n<k){
-        step=diff;
-    }else{
-        step = (diff+1)/2;
-    }
-    cout  << step << nl;
+    ll n,m,k;
+    cin >> n >> m >> k;
+    ll mul =m*k;
+    // ll ans = 0;
+    // ll amount= ((k+1)/2)-1;
+    // ll bar=0;
+    // if(amount){
+    //     bar=min(mul/amount,n-1);
+    // }
+    // mul-=bar*amount;
+    // ans+=bar*amount;
+    // ll r = mul % k;
+    // if(r>=(k+1)/2){
+    //     ans-=k-r;
+    // }else{
+    //     ans+=mul-(mul/k)*k;
+    // }
+    // cout << ans << nl;
+    ll rem = ((k+1)/2-1)*n;
+    ll coins = (mul-rem+k-1)/k;
+    coins = max(0LL,coins);
+    ll rem2 =mul - coins *k;
+    cout << rem2 << nl;
 }
 int main(){
     Tarek
