0ab5fae17185284eb075a97925c5e28e93f3a0c1
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-07-09 23:10:44 +0600

    Add files via upload

---
+++ B_Maximum_Strength.cpp
@@ -0,0 +1,83 @@
+/*
+               Starting with the name of almighty ALLAH 
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**
+            In the name of Allah, Most Gracious, Most Merciful             
+                    লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ 
+                        Author:  tarek200 
+                        Created: 07.07.2023 
+                    United International University
+                Practice is the only shortcut to improve  
+*/
+#include <bits/stdc++.h>
+#include<vector>
+#include<set>
+#define   Tarek           ios::sync_with_stdio(false);cin.tie(0);
+#define   ll              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int>
+#define   vll             vector<ll>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   test            ll t;cin >> t;while(t--)
+#define   pb              push_back
+#define   pp              pop_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   checkmate       return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   mod             1000000007
+#define   all(x)          (x).begin(), (x).end()
+#define   des(x)          (x).rbegin(), (x).rend()
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   FOR(i,a,b)      for(ll i=a;i<b;i++)
+#define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
+#define   LOOP2(i,a,b)    for(ll i=a;i<=b;i++)
+#define   ROF(i, a, b)    for (ll i = a; i >=b; i--)
+const long long int N     = (ll) 1e7+1;
+const long long int M     = (ll) 1e9+5;
+#define MAX                 200005
+#define MOD                 1000003
+#define eps                 1e-9
+#define INF                 0x3f3f3f3f3f3f3f3f // 4,557,430,888,798,830,399
+#define inf                 0x3f3f3f3f // 1,061,109,567
+using namespace std;
+void run(){
+    string start,end;       
+    cin >> start >> end;
+    reverse(all(start));
+    reverse(all(end));
+    while(start.sz<end.sz){
+        start+="0";
+    }
+    reverse(all(start));
+    reverse(all(end));
+    bool flag=true;
+    ll ans=0;
+    FOR(i,0,start.sz){
+        if(!flag){
+            ans+=9LL;
+            next
+        }
+        if(start[i]!=end[i]){
+            ans+=(abs(start[i]-end[i]));
+            flag=false;
+        }
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    Tarek
+    test{
+        run();
+    }
+    checkmate
+}
