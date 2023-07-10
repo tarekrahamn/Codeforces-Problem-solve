46d59b02f3657844740ef252c8d6dcaf75d78741
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-07-10 22:03:17 +0600

    Add files via upload

--- A_Way_Too_Long_Words.cpp
+++ A_Way_Too_Long_Words.cpp
@@ -1,45 +1,68 @@
 /*
-**************** লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ *****************************\\
-**************** author:  tarek200 *************************\\
-**************** created: 16.03.2023 ************************\\
-*
+########## Starting with the name of almighty ALLAH #######################
+**************** লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ ***********
+**************** Author:  tarek200 *************************
+**************** Created: 23.05.2023 ***********************
+$$$$$$$$$$ Practice is the only shortcut to improve $$$$$$$$$$$$$$$$$$$$$$$$
 **/
 #include <bits/stdc++.h>
+#define   Tarek           ios::sync_with_stdio(false);cin.tie(0);
+#define   ll              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int>
+#define   vll             vector<ll>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   test            ll t;cin >> t;while(t--)
+#define   push            push_back
+#define   pop             pop_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   B               break;
+#define  checkmate        return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   mod             1000000007
+#define   PI              3.14159265358979323846
+#define   all(x)         (x).begin(), (x).end()
+#define   rev(dp)         reverse(dp.begin(), dp.end());
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   FOR(i,a,b)      for(ll i=a;i<b;i++)
+#define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
+#define   LOOP2(i,a,b)    for(ll k=a;k<=b;k++)
+const long long int N     = (ll) 1e7+1;
+const long long int M     = (ll) 1e9+5;
 using namespace std;
-#define     ll                                long long int
-#define     ull                               unsigned long long int
-#define     pb                                push_back
-const int   N                               = (int) 1e6+5;
-const int   M                               = (int) 1e9+5;
-const int   MOD                             = (int) 1000000007;
-const int   max_prime                       = (int) 1e6+3;
-const int   BLK                             = (int) 700;
-const int   X                               = 100000;
-// PRIME NUMBER TEST//
-bool prime(int n){
-    if(n<2)return false;
-    if(n<=3)return true;
-    if(n%2==0)return false;
-    for(int i=3; i*i<=n; i+=2){
-        if(n%i==0)return false;
-    }
-    return true;
-}
-void UIU(){
+/* 
+this problem task is we have string .if the string length is less then 10 then wr print the
+string.other wishe wee need to print first and last character of the string and need to 
+find middle length.
+like this, 
+input : UNITED 
+output:U4D
+
+
+*/
+void run(){
     string s;
     cin >> s;
-    int n = s.length();
-    if(n<=10){
+    if(s.sz<=10){
         cout << s << endl;
     }else{
-        cout << s[0]<< s.size()-2 <<s[s.size()-1] << endl;
+        cout << s[0] << s.length()-2 <<s[s.length()-1] << nl;
     }
 }
 int main(){
-    int t;
-    scanf("%d",&t);
-    while(t--){
-        UIU();
+    Tarek
+    test{
+        run();
     }
-    return 0;
 }
--- B_Hossam_and_Friends.cpp
+++ B_Hossam_and_Friends.cpp
@@ -1,56 +1,98 @@
 /*
-**************** লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ *****************************\\
-**************** author:  tarek200 *************************\\
-**************** created: 16.03.2023 ************************\\
-*
+########## Starting with the name of almighty ALLAH #######################
+**************** লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ ***********
+**************** Author:  tarek200 *************************
+**************** Created: 23.05.2023 ***********************
+$$$$$$$$$$ Practice is the only shortcut to improve $$$$$$$$$$$$$$$$$$$$$$$$
 **/
 #include <bits/stdc++.h>
+#define   Tarek           ios::sync_with_stdio(false);cin.tie(0);
+#define   ll              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int>
+#define   vll             vector<ll>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   test            ll t;cin >> t;while(t--)
+#define   push            push_back
+#define   pop             pop_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   B               break;
+#define  checkmate        return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   mod             1000000007
+#define   PI              3.14159265358979323846
+#define   all(x)         (x).begin(), (x).end()
+#define   rev(dp)         reverse(dp.begin(), dp.end());
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   FOR(i,a,b)      for(ll i=a;i<b;i++)
+#define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
+#define   LOOP2(i,a,b)    for(ll k=a;k<=b;k++)
+const long long int N     = (ll) 1e7+1;
+const long long int M     = (ll) 1e9+5;
 using namespace std;
-#define     ll                                long long int
-#define     ull                               unsigned long long int
-#define     pb                                push_back
-const int   N                               = (int) 1e6+5;
-const int   M                               = (int) 1e9+5;
-const int   MOD                             = (int) 1000000007;
-const int   max_prime                       = (int) 1e6+3;
-const int   BLK                             = (int) 700;
-const int   X                               = 100000;
-// PRIME NUMBER TEST//
-bool prime(int n){
-    if(n<2)return false;
-    if(n<=3)return true;
-    if(n%2==0)return false;
-    for(int i=3; i*i<=n; i+=2){
-        if(n%i==0)return false;
-    }
-    return true;
-}
-void UIU(){
-    int x,y;
-    scanf("%d%d",&x,&y);
-    ll ans = 0;
-    int mx[x+2] = {0};
-    for(int i=0;i<y;i++){
-        int a,b;
-        scanf("%d%d",&a,&b);
-        if(a>b){
-            swap(a,b);
-            mx[b] = max(mx[b], a);
+/*
+In ths problem our task is there was give 1 to n friends.and give some pair that means there was some friend
+dose not have friendship whith them.now need to say how manay subsegment we build that there was no unfried 
+ferson in same subsagment .
+like N=6 and dose't frient 2 
+(3,5)and (3,6) they are not friend 
+
+1 2 3 4 5 6
+now i need to say how many segment we make .without 
+3 4 5 and 3 4 5 6
+
+Now what is subsegment 
+1 2 3 4 5 6 the subsegment is some part of array
+such as  1 2 , 5 6 and  2 3
+
+now some manul input :
+n = 3 and m = 2 (2,3)they are not friends
+1 2 3
+there was 4 good pairs that is  1 , 2  , 3 and 1 2
+lest impelement this 
+*/
+void run(){
+    ll n,m;
+    cin >> n >> m;
+    vll a(n+10);
+    // Unfried person
+    FOR(i,0,m){
+        ll p , q;
+        cin >> p >> q;
+        /*
+        there was a problem
+        1 2 3 4 5 6 and unfriend peope are (3,6) and (4,6)
+        at this thime need closest max number of 6
+        */
+        if(p>q){
+            swap(p,q);
+            // a[q]=max(a[q],p);
         }
+        a[q]=max(a[q],p);
     }
-    for(int i = 1; i <= x; i++){
-        mx[i] = max(mx[i], mx[i-1]);
+    // how many segment we can make
+    ll x =1;
+    ll ans = 0;
+    LOOP2(k,1,n){
+        x =max(x,(ll)a[k]+1LL);
+        ans +=(k-x+1);
     }
-    for(int i = 1; i <= x; i++){
-        ans += i-mx[i];
-    }
-    printf("%d\n",ans);
+    cout << ans << nl;
 }
 int main(){
-    int t;
-    scanf("%d",&t);
-    while(t--){
-        UIU();
+    Tarek
+    test{
+        run();
     }
-    return 0;
 }
