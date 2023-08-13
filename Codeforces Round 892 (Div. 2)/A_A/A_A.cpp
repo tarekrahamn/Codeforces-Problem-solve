f94d4e898f719af5e479559317d57adebc4cf716
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-08-13 18:35:43 +0600

    Add files via upload

---
+++ Codeforces Round 892 (Div. 2)/A_A/A_A.cpp
@@ -0,0 +1,124 @@
+/*Starting with the name of almighty ALLAH 
+Author:  Tarek Rahman
+Created: 12.08.2023 
+United International University 
+*/
+ 
+#include <bits/stdc++.h>
+#define   int              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int32_t>
+#define   vll             vector<int>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   vb              vector<bool>
+#define   pb              push_back
+#define   ppb             pop_back
+#define   mp              make_pair
+#define   eb              emplace_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   checkmate       return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   MOD             1000000007
+#define   MOD1            998244353
+#define   INF             1e18
+#define   PI              3.141592653589793238462
+#define   dbug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define   all(x)          (x).begin(), (x).end()
+#define   des(x)          (x).rbegin(), (x).rend()
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   print1(a)       for(auto x : a) cout << x.ff << " " << x.ss << endl
+using namespace std;
+// PRIME NUMBER TEST//
+bool prime(int n){
+    if(n<2)return false;
+    if(n<=3)return true;
+    if(n%2==0)return false;
+    for(int i=3; i*i<=n; i+=2){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+//Exponent function 
+int expo(int a, int b, int mod){
+    int res = 1;
+    while (b>0){
+        if(b & 1){
+            res = (res *a) % mod;
+        }
+        a = (a*a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void run(int t){
+    int n;
+    cin >> n;
+    vll a(n);
+    map<int, int> fr;
+    int maxval = INT_MIN, cnt = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        maxval = max(maxval, a[i]);
+    }
+     vll b, c;
+    
+    for (int i = 0; i < n; ++i) {
+        if (a[i] == maxval and cnt == 0) {
+                c.pb(a[i]);
+        } else {
+                b.pb(a[i]);
+            }
+        }
+        if(b.sz==0){
+            cout << -1 << nl;
+        }else {
+            cout << b.sz << " " << c.sz << nl;
+            for (int num : b) {
+                cout << num << " ";
+            }
+            cout << nl;
+            for (int num : c) {
+                  cout << num << " ";
+        }
+        cout << nl;
+    }
+    
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    clock_t z = clock();
+    int t = 1;
+    cin >> t;
+    while (t--){
+        run(t);
+    }
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
