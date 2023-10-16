a37821d4098b63e8cdfa4ab9ccd18b6eb70a32b4
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-10-16 22:14:46 +0600

    Add files via upload

---
+++ Codeforces Round 903 (Div. 3)/B_Three_Threadlets.cpp
@@ -0,0 +1,103 @@
+/*Starting with the name of almighty ALLAH
+Author:  Tarek Rahman
+Created: 12.10.2023
+United International University
+*/
+#include <bits/stdc++.h>
+#define   int             long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int32_t>
+#define   vll             vector<int>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   vb              vector<bool>
+#define   pb              push_back
+#define   ppb             pop_back
+#define   mp              make_pair
+#define   eb              emplace_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   ONLINE_JUDGE
+#define   checkmate       return 0;
+#define   nl              "\n"
+#define   MOD             1000000007
+#define   MOD1            998244353
+#define   INF             1e18
+#define   PI              3.141592653589793238462
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a*b)/gcd(a,b)
+#define   dbug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define   all(x)          (x).begin(), (x).end()
+#define   des(x)          (x).rbegin(), (x).rend()
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   fo(i,a,b)      for(int i=a;i<b;i++)
+#define   fora(i,a,b)     for(int j=a;j<b;j++)
+#define   fora2(i,a,b)    for(int i=a;i<=b;i++)
+#define   rof(i, a, b)    for (int i = a; i >=b; i--)
+const int N = (int) 1e5 + 10;
+const int X = (int) 1e8;
+using namespace std;
+
+//-----------------------------------------------------------------------------------------------------//
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+//-----------------------------------------------------------------------------------------------------//
+
+void run(int t) {
+    vector<int> a(3, 0);
+    fo(i,0,3){
+        cin >> a[i];
+    }
+    sort(all(a));
+    bool flag=false;
+    int target=a[2];
+     int first = a[0];
+    if (a[0] == a[1]) {
+        fora2 (i,0,3) {
+            if (first * (i + 1) == target) {
+                flag=true;
+                break;
+            }
+        }
+    }
+    if (a[0] * 2 == a[1]) {
+        fora2 (i,0,2) {
+            if (first * (i + 1) == target) {
+                flag=true;
+                break;
+            }
+        }
+    }
+    if (!flag) {
+        NO
+    } else {
+        YES
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input2.txt",  "r",  stdin);
+    freopen("output2.txt", "w", stdout);
+#endif
+  //  clock_t z = clock();
+    int t = 1;
+    cin >> t;
+    while (t--){
+        run(t);
+    }
+   // cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
+First row ko first column se match Karo then same for the second ...for all elements of the row and col ..ye kam karega kya,?
---
+++ Codeforces Round 903 (Div. 3)/D_Divide_and_Equalize.cpp
@@ -0,0 +1,103 @@
+/*Starting with the name of almighty ALLAH
+Author:  Tarek Rahman
+Created: 12.10.2023
+United International University
+*/
+#include <bits/stdc++.h>
+#define   int             long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int32_t>
+#define   vll             vector<int>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   vb              vector<bool>
+#define   pb              push_back
+#define   ppb             pop_back
+#define   mp              make_pair
+#define   eb              emplace_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   ONLINE_JUDGE
+#define   checkmate       return 0;
+#define   nl              "\n"
+#define   MOD             1000000007
+#define   MOD1            998244353
+#define   INF             1e18
+#define   PI              3.141592653589793238462
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a*b)/gcd(a,b)
+#define   dbug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define   all(x)          (x).begin(), (x).end()
+#define   des(x)          (x).rbegin(), (x).rend()
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   fo(i,a,b)      for(int i=a;i<b;i++)
+#define   fora(i,a,b)     for(int j=a;j<b;j++)
+#define   fora2(i,a,b)    for(int i=a;i<=b;i++)
+#define   rof(i, a, b)    for (int i = a; i >=b; i--)
+const int N = (int) 1e5 + 10;
+const int X = (int) 1e8;
+using namespace std;
+
+//-----------------------------------------------------------------------------------------------------//
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+//-----------------------------------------------------------------------------------------------------//
+
+void run(int t) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    fo(i,0,n) {
+        cin >> a[i];
+    }
+    map<int, int> factor;
+    fo(i,0,n) {
+        int need = a[i];
+        for (int i = 2; i*i <= need; i++) {
+            while (need % i == 0) {
+                need /= i;
+                factor[i]++;
+            }
+        }
+        if (need != 1) {
+            factor[need]++;
+        }
+    }
+    int maxx = 0;
+    for (const auto &i : factor) {
+        maxx = max(maxx, i.ss);
+    }
+    for (const auto &pair : factor) {
+        if (pair.ss % n != 0) {
+            NO
+            return;
+        }
+    }
+    YES
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input2.txt",  "r",  stdin);
+    freopen("output2.txt", "w", stdout);
+#endif
+    clock_t z = clock();
+    int t = 1;
+    cin >> t;
+    while (t--){
+        run(t);
+    }
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
